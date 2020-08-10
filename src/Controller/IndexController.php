9e678328bf738047c2bf3afbff4439ffb109bbfd
antonia11yb <antonia.yb@gmail.com>
2020-08-10 21:02:32 +0300

    create first composer

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,8 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-        
+    /**
+     * @Route("/")
+     */
+    public function home()
+    {
+        return new Response(
+            '<h1>My first Symfony Page</h1>'
+        );
+    }
+
 }
